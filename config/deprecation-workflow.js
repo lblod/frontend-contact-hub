2af24528e456c45a14c1e05f998502f02f212576
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2021-07-27 20:34:17 +0200

    Update the deprecation workflow config
    Ember 3.27 introduces more deprecation that are common in addons.

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,9 +2,20 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    // ember-modifiers v1 triggers this, which is bundled by ember-appuniversum
+    // ember-focus-trap < v0.6.0 triggers this, which is bundled by ember-appuniversum
     { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' },
     { handler: 'silence', matchId: 'this-property-fallback' }, // AuDataTable triggers this
     { handler: 'silence', matchId: 'implicit-injections' }, // FastBoot triggers this
+    // ember-inspector triggers this
+    {
+      handler: 'silence',
+      matchId: 'deprecated-run-loop-and-computed-dot-access',
+    },
+    {
+      handler: 'silence',
+      matchId: 'ember.built-in-components.import',
+    },
+    { handler: 'silence', matchId: 'ember.component.reopen' }, // ember-test-selectors < v6 triggers this which is included by ember-appuniversum
+    { handler: 'silence', matchId: 'ember-global' },
   ],
 };
